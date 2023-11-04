154f5019538f031b88db228939893cd2c77a1f4a
Kip Thompson-Born <kipthompsonborn@hotmail.com>
2023-11-04 13:26:02 +0000

    Updating Milestone_3 files.
    This commit adds Milestone 3 Task 2 additions.

--- wip/milestone_3.py
+++ wip/milestone_3.py
@@ -1,13 +1,22 @@
 ### MILESTONE 3
+import random
 
-"""This code will continuously ask the user for a letter and validate it."""
+word_list = ["Apple", "Orange", "Kiwi", "Blueberries", "Raspberries"]
+
+word = random.choice(word_list)
 
 guess = input("What letter would you like to guess?")
 
+# This code will continuously ask the user for a letter and validate it.
 while True:
     guess = input("What letter would you like to guess?")
     if len(guess) != 1 or guess.isalpha == False:
         print("Invalid letter. Please, enter a single alphabetical character.")
     else:
         break
+
+if guess in word:
+    print(f"Good guess! {guess} is in the word.")
+else:
+    "Sorry, {guess} is not in the word. Try again."
 
