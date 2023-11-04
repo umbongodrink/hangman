1c4d567e19aafc97ae44b899486f77f26fa5b7d0
Kip Thompson-Born <kipthompsonborn@hotmail.com>
2023-11-04 14:00:11 +0000

    Updating milestone_3.py to include function integration from Milestone 3 Task 3.
    The two functions are:
    
    check_guess()
    and
    ask_for_input()

--- wip/milestone_3.py
+++ wip/milestone_3.py
@@ -1,22 +1,29 @@
 ### MILESTONE 3
 import random
 
-word_list = ["Apple", "Orange", "Kiwi", "Blueberries", "Raspberries"]
+word_list = ["Apple", "Mango", "Kiwi", "Blueberries", "Raspberries"]
 
 word = random.choice(word_list)
 
-guess = input("What letter would you like to guess?")
-
-# This code will continuously ask the user for a letter and validate it.
-while True:
-    guess = input("What letter would you like to guess?")
-    if len(guess) != 1 or guess.isalpha == False:
-        print("Invalid letter. Please, enter a single alphabetical character.")
+# This function will check if player1's letter is in the chosen word.
+def check_guess(guess):
+    guess = guess.lower()
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
     else:
-        break
+        print(f"Sorry, {guess} is not in the word. Try again.")
 
-if guess in word:
-    print(f"Good guess! {guess} is in the word.")
-else:
-    "Sorry, {guess} is not in the word. Try again."
+# This code will check if player1's guess if valid. 
+def ask_for_input():
+    while True:
+        guess = input("What letter would you like to guess?")
+        if len(guess) != 1 or guess.isalpha == False:
+            print("Invalid letter. Please, enter a single alphabetical character. ")
+        else:
+            break
+    
+    # It then plays the game by calling the check_guess()function
+    check_guess(guess)
 
+# The game commences with the ask_for_input() function call.
+ask_for_input()
