a2332dfdc01f076d196beeb60bca267d655861cb
Kip Thompson-Born <kipthompsonborn@hotmail.com>
2023-11-05 17:04:51 +0000

    Adding updated Milestone_4.py and Milestone_4.ipynb
    Time is now 17.05, and currently working through Milestone 4 task 3.
    The current issue is what happens if the letter is capitalised.

--- wip/milestone_4.py
+++ wip/milestone_4.py
@@ -13,3 +13,25 @@ class Hangman:
         self.num_letters = len(set(self.word.lower()))
         self.list_of_guesses = []
 
+    def check_guess(self, guess):
+        guess = guess.lower()
+        if guess in self.word:
+            print(f"Good guess! {guess} is in the word.")
+            for _ in self.word:
+                if _ == guess:
+                    index_value = self.word.index(_)
+                    print(index_value)
+                    self.word_guessed[index_value] = guess
+                    print(self.word_guessed)
+
+    def ask_for_input(self):
+        while True:
+            guess = input("Please choose a letter...")
+            print(guess)
+            if len(guess) != 1 or guess.isalpha == False:
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter!")
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)
