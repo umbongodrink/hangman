e2b2ef95e54555226f596d42c612e718ef10b190
Kip Thompson-Born <kipthompsonborn@hotmail.com>
2023-11-06 08:59:00 +0000

    Updating milestone_4.py and milestone_4.ipynb on completing Milestone 4 Task 4.
    NOTE: the code is a little unfinished:
    - the game doesn't end or print a message if you guess all letters correctly.
    - sometimes it gets into a loop and doesn't break out properly - what happens if you guess a non single or non-alphabetical letter
    - the game doesn't take into account the use case for Capital letters at the start of words.

--- wip/milestone_4.py
+++ wip/milestone_4.py
@@ -20,9 +20,13 @@ class Hangman:
             for index, letter in enumerate(self.word):
                     if letter == guess:
                         self.word_guessed[index] = guess
-            #print(self.word_guessed)
+            #rint(self.word_guessed)
             self.num_letters -= 1
             #print(self.num_letters)
+        else:
+            self.num_lives -= 1
+            print(f"Sorry, {guess} is not in the word.")
+            print(f"You have {self.num_lives} lives left.")
 
     def ask_for_input(self):
         while True:
